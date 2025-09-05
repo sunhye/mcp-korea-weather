ebe145c69243723e6ce2f031bae4353996790281
sunhye <comggi@gmail.com>
2025-09-05 12:36:17 +0900

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -35,10 +35,15 @@ export default function createServer({
     })
 
     // Add a tool
-    server.tool(
+    server.registerTool(
         "get_korea_weather",
-        "기상청 초단기실황(기온/강수/풍속/습도)",
         {
+            title: "get_korea_weather",
+            description: "기상청 초단기실황(기온/강수/풍속/습도)",
+            inputSchema: {
+                latitude: z.string(),
+                longitude: z.string()
+            },
         },
         async (args, context) => {
             const {latitude, longitude} = args as { latitude: string; longitude: string };
@@ -53,12 +58,17 @@ export default function createServer({
                 ]
             };
         },
-    );
+    )
 
-    server.tool(
+    server.registerTool(
         "get_korea_forecast",
-        "기상청 초단기예보(향후 3개 타임슬롯 요약)",
         {
+            title: "get_korea_forecast",
+            description: "기상청 초단기예보(향후 3개 타임슬롯 요약)",
+            inputSchema: {
+                latitude: z.string(),
+                longitude: z.string()
+            },
         },
         async (args, context) => {
             const { latitude, longitude } = args as { latitude: string; longitude: string };
@@ -70,7 +80,7 @@ export default function createServer({
                 ]
             };
         }
-    );
+    )
 
 
     return server.server
